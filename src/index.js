c81b8e30bd8999476208a3d6dfdfdfaffd3f87a7
Pedro Madruga <pedro@occam.ooo>
2017-11-18 22:23:50 +0100

    feature: react+electron

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+const electron = window.require('electron');
+const fs = electron.remote.require('fs');
+const ipcRenderer = electron.ipcRenderer;
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
